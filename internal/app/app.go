66331f413a7eaf5056c86e44bb7a7f0e25131f95
chipazawra <chipazawra@mail.ru>
2021-12-28 14:23:42 +0300

    rpHosts collector

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,21 +1,25 @@
 package app
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/Chipazawra/v8-1c-cluster-pde/internal/rpHostsCollector"
 	"github.com/caarlos0/env"
-	rclient "github.com/khorevaa/ras-client"
+	rascli "github.com/khorevaa/ras-client"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
-	conf     Config
-	hostFlag string
-	portFlag string
+	conf       Config
+	hostFlag   string
+	portFlag   string
+	exposeFlag string
+	ctx        context.Context
 )
 
 func init() {
@@ -25,6 +29,7 @@ func init() {
 
 	flag.StringVar(&hostFlag, "host", "", "cluster host.")
 	flag.StringVar(&portFlag, "port", "", "cluster port.")
+	flag.StringVar(&exposeFlag, "expose", "", "metrics port.")
 	flag.Parse()
 
 	if hostFlag != "" {
@@ -34,36 +39,36 @@ func init() {
 	if portFlag != "" {
 		conf.Port = portFlag
 	}
+
+	if exposeFlag != "" {
+		conf.Expose = exposeFlag
+	}
+
+	ctx = context.Background()
+
 }
 
 func Run() error {
 
-	_ = rclient.NewClient(fmt.Sprintf("%s:%s", conf.Host, conf.Port))
+	rcli := rascli.NewClient(fmt.Sprintf("%s:%s", conf.Host, conf.Port))
+	rcli.AuthenticateAgent(conf.User, conf.Pass)
+	defer rcli.Close()
 
 	promRegistry := prometheus.NewRegistry()
-	promRegistry.MustRegister()
-	_ = prometheus.NewGauge(
-		prometheus.GaugeOpts{
-			Namespace:   "",
-			Subsystem:   "",
-			Name:        "",
-			Help:        "",
-			ConstLabels: map[string]string{},
-		})
+	promRegistry.MustRegister(rpHostsCollector.New(rcli))
 
-	_ = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Namespace:   "",
-			Subsystem:   "",
-			Name:        "",
-			Help:        "",
-			ConstLabels: map[string]string{},
-		},
-		nil,
+	http.Handle("/metrics",
+		promhttp.HandlerFor(promRegistry, promhttp.HandlerOpts{}),
 	)
 
-	http.Handle("/metrics", promhttp.HandlerFor(promRegistry, promhttp.HandlerOpts{}))
+	err := http.ListenAndServe(fmt.Sprintf("%s:%s", "", conf.Expose), nil)
+	if err != nil {
+		return Errorf(err)
+	}
 
-	return fmt.Errorf("app: not implemented")
+	return nil
+}
 
+func Errorf(err error) error {
+	return fmt.Errorf("app: %v", err)
 }
--- internal/app/config.go
+++ internal/app/config.go
@@ -1,8 +1,9 @@
 package app
 
 type Config struct {
-	Host string `env:"HOST_1C" envDefault:"localhost"`
-	Port string `env:"PORT_1C" envDefault:"1545"`
-	User string `env:"USER_1C"`
-	Pass string `env:"PASS_1C"`
+	Host   string `env:"HOST_1C" envDefault:"localhost"`
+	Port   string `env:"PORT_1C" envDefault:"1545"`
+	User   string `env:"USER_1C"`
+	Pass   string `env:"PASS_1C"`
+	Expose string `env:"EXPOSE" envDefault:"9096"`
 }
